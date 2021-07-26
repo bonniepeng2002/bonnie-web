dbe4a5bc4e840b36525b43a8e7f6e8bcb636bb51
bonniepeng2002 <bonniepeng2002@gmail.com>
2021-07-26 15:19:18 -0300

    fixes

--- pages/Contact.tsx
+++ pages/Contact.tsx
@@ -6,6 +6,8 @@ import Email from "../public/svg/shells/email-shell.svg";
 import Github from "../public/svg/shells/gh-shell.svg";
 import styles from "../styles/Contact.module.scss";
 
+/* <AttentionSeeker effect="bounce"></AttentionSeeker> */
+
 const resumeLink =
   "https://drive.google.com/file/d/1Hzm4FGKQruiG8JSnD1Q036jsgGKElbAt/preview";
 
@@ -18,33 +20,29 @@ export default function Contact() {
           <h1 className={styles.header}>Let's get in touch!</h1>
           <div className={styles.shell_container}>
             <div className={styles.shell_container1}>
-              <AttentionSeeker effect="bounce" cascade={true}>
-                <a
-                  className={styles.linkedin}
-                  href="https://www.linkedin.com/in/bonniepeng/"
-                  target="_blank"
-                >
-                  <img src={LinkedIn} alt="LinkedIn" className={styles.shell} />
-                </a>
-                <a
-                  className={styles.github}
-                  href="https://github.com/bonniepeng2002"
-                  target="_blank"
-                >
-                  <img src={Github} alt="Github" className={styles.shell} />
-                </a>
-              </AttentionSeeker>
+              <a
+                className={styles.linkedin}
+                href="https://www.linkedin.com/in/bonniepeng/"
+                target="_blank"
+              >
+                <img src={LinkedIn} alt="LinkedIn" className={styles.shell} />
+              </a>
+              <a
+                className={styles.github}
+                href="https://github.com/bonniepeng2002"
+                target="_blank"
+              >
+                <img src={Github} alt="Github" className={styles.shell} />
+              </a>
             </div>
-            <AttentionSeeker effect="bounce">
-              <div className={styles.shell_container2}>
-                <a
-                  className={styles.email}
-                  href="mailto:bonnie.peng@uwaterloo.ca"
-                >
-                  <img src={Email} alt="Email" className={styles.shell} />
-                </a>
-              </div>
-            </AttentionSeeker>
+            <div className={styles.shell_container2}>
+              <a
+                className={styles.email}
+                href="mailto:bonnie.peng@uwaterloo.ca"
+              >
+                <img src={Email} alt="Email" className={styles.shell} />
+              </a>
+            </div>
           </div>
         </div>
         <iframe
