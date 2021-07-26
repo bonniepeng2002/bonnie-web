99b6691e0de5b29ec030d13150c59d8c34b0d548
bonniepeng2002 <bonniepeng2002@gmail.com>
2021-07-26 16:55:39 -0300

    customize tab

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,20 @@
-import '../styles/globals.scss';
+import Head from "next/head";
+import "../styles/globals.scss";
 import "../styles/customTimeline.css";
 
-import { AppProps } from 'next/app';
-import React from 'react';
+import { AppProps } from "next/app";
+import React from "react";
 
 function App({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link rel="icon" type="image/png" sizes="32x32" href="/icon.png" />
+        <title>Bonnie Peng</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default App;
